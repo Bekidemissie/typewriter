179e23e07106119262c2ee09dcf72ff5aeb758f2
Bekidemissie <bekidemissie16@gmail.com>
2023-07-21 21:57:40 -0400

    frist commite

---
+++ typewriter.js
@@ -0,0 +1,7 @@
+const sentence = "hello there from lighthouse labs";
+for (const char of sentence) {
+    console.log(char);
+  }
+  setTimeout(() => {
+    // print the char here
+  }, 1000) // <= 
